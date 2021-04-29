2103921c1e2c84ea4f46e09c4a775233eb761796
Joey JEANTET <jeantetjoey.pro@gmail.com>
2021-04-29 16:06:28 +0200

    Ajout bdd et méthode de récupération des objets de la bdd

--- src/extensions/simulation/controller.ts
+++ src/extensions/simulation/controller.ts
@@ -2,7 +2,7 @@ import {extension} from "../../app/extensionController";
 import { spawn } from 'child_process';
 import path from "path";
 import * as process from "process";
-
+import PouchDB from "pouchdb";
 export default class simulationExtension extends extension {
 
     async before() {
--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,7 @@
 import {Server} from "socket.io";
-import instance from "./instance";
+import PouchDB from 'pouchdb';
 import simulationExtension from "./extensions/simulation/controller";
+import {val} from "objection";
 
 export default function(io: Server){
     io.sockets.on('connection', function(socket) {
@@ -15,9 +16,39 @@ export default function(io: Server){
             console.log('user disconnected');
         });
 
+        socket.on('getSimulationById', (id) => {
+            const db = new PouchDB('simulations');
+            db.get(id).then((obj) => {
+                socket.emit("simulationById", obj)
+            }).catch(() => {
+                socket.emit("error", "Aucune simulation ne possède cet identifiant")
+            })
+        });
+
+        socket.on('getSimulations', () => {
+            const db = new PouchDB('simulations');
+            db.allDocs({
+                include_docs: true,
+                attachments: true
+            }).then(function (result) {
+                var toSend: Array<any> = [];
+                result.rows.forEach(value => {
+                    toSend.push({
+                        // @ts-ignore
+                        date: value.doc.date,
+                        id: value.id
+                    })
+                })
+                socket.emit('simulationList', toSend)
+            }).catch(function (err) {
+                console.log(err);
+            });
+        })
+
+
         socket.on('run-simulation', (data) => {
-            var list: Array<any> = [];
-            var sim = simulationExtension.runSimulation();
+            const list: Array<any> = [];
+            const sim = simulationExtension.runSimulation();
             data.forEach((line: any) => {
                 sim.stdin.write(JSON.stringify(line));
             })
@@ -25,12 +56,20 @@ export default function(io: Server){
             sim.stdout.on('data', (data) => {
                 try {
                     data = JSON.parse(data.toString())
+                    console.log(data);
                     list.push(data)
                 }catch (e) {
                     console.log(e);
                 }
             });
+
+
             sim.stdout.on("close", ()=> {
+                const db = new PouchDB('simulations');
+                db.post({
+                    date: Date.now().toString(),
+                    sim: list
+                });
                 socket.emit("simulation-result", list)
             })
 
